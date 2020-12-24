224f1dafcf5d57bcaa550451f1429a0d846aadf2
byte-yaoyao <2282580415@qq.com>
2020-12-24 15:45:56 +0800

    variable

---
+++ Adding/src/Adding.java
@@ -0,0 +1,17 @@
+public class Adding {
+    public static void main(String[] args) {
+        int m;
+        int n;
+           m = 5;
+           n = 2;
+       // m = Integer.MIN_VALUE;
+      //  m = Integer.MAX_VALUE;
+       // n = Long.MIN_VALUE;
+      //  n = Long.MAX_VALUE;
+        //System.out.println(Integer.MIN_VALUE);
+      //  System.out.println(Integer.MAX_VALUE);
+      //  System.out.println(Long.MAX_VALUE);
+      //  System.out.println(Long.MAX_VALUE);
+        System.out.println(m * n);
+    }
+}
